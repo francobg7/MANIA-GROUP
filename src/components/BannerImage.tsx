bd3cbe51876c8da6ed9a893fb95a8ecc4525fdc2
francobg7 <francobg7@gmail.com>
2025-11-04 13:48:04 -0300

    pic added

---
+++ src/components/BannerImage.tsx
@@ -0,0 +1,26 @@
+interface BannerImageProps {
+  src: string;
+  alt?: string;
+  className?: string;
+}
+
+const BannerImage = ({ src, alt = "Banner promocional", className = "" }: BannerImageProps) => {
+  return (
+    <section className={`w-full my-8 md:my-12 ${className}`}>
+      <div className="w-full overflow-hidden">
+        <img
+          src="/images/logos/BANNER.png"
+          alt="Banner promocional especial"
+          className="w-full h-auto object-cover"
+          style={{
+            aspectRatio: "1920 / 600",
+            maxHeight: "600px",
+          }}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default BannerImage;
+
