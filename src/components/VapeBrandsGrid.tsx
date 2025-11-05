b5c2449cafee108ddde54c531630a775bb46ad17
francobg7 <francobg7@gmail.com>
2025-11-05 17:16:13 -0300

    more components added

--- src/components/VapeBrandsGrid.tsx
+++ src/components/VapeBrandsGrid.tsx
@@ -2,22 +2,24 @@ import { Product } from "@/contexts/CartContext";
 import VapeBrandCard from "./VapeBrandCard";
 import BlackSheepGroup from "./BlackSheepGroup";
 import ElfBarGroup from "./ElfBarGroup";
+import GeekBarGroup from "./GeekBarGroup";
 
 interface VapeBrandsGridProps {
   products: Product[];
 }
 
 const VapeBrandsGrid = ({ products }: VapeBrandsGridProps) => {
-  // Separar productos de Black Sheep y Elf Bar de los demás
+  // Separar productos de Black Sheep, Elf Bar y Geek Bar de los demás
   const blackSheepProducts = products.filter((p) => p.brand === "Black Sheep");
   const elfBarProducts = products.filter((p) => p.brand === "Elf Bar");
+  const geekBarProducts = products.filter((p) => p.brand === "Geek Bar");
   
-  // Obtener productos que NO son de Black Sheep ni Elf Bar
+  // Obtener productos que NO son de Black Sheep, Elf Bar ni Geek Bar
   const otherProducts = products.filter(
-    (p) => p.brand !== "Black Sheep" && p.brand !== "Elf Bar"
+    (p) => p.brand !== "Black Sheep" && p.brand !== "Elf Bar" && p.brand !== "Geek Bar"
   );
 
-  // Agrupar productos por marca (excluyendo Black Sheep y Elf Bar)
+  // Agrupar productos por marca (excluyendo Black Sheep, Elf Bar y Geek Bar)
   const productsByBrand = otherProducts.reduce((acc, product) => {
     const brand = product.brand;
     if (!acc[brand]) {
@@ -42,6 +44,11 @@ const VapeBrandsGrid = ({ products }: VapeBrandsGridProps) => {
           {/* Renderizar Elf Bar como grupo unificado si tiene productos */}
           {elfBarProducts.length > 0 && (
             <ElfBarGroup products={elfBarProducts} />
+          )}
+          
+          {/* Renderizar Geek Bar como grupo unificado si tiene productos */}
+          {geekBarProducts.length > 0 && (
+            <GeekBarGroup products={geekBarProducts} />
           )}
           
           {/* Renderizar demás marcas */}
