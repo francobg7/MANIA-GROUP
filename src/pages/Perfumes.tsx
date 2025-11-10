3609ae6f366d3d907320a3c8980df228cbde510b
francobg7 <francobg7@gmail.com>
2025-11-10 11:58:39 -0300

    modifications for device

--- src/pages/Perfumes.tsx
+++ src/pages/Perfumes.tsx
@@ -58,7 +58,7 @@ const Perfumes = () => {
               />
             </aside>
             <div className="flex-1">
-              <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+              <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
                 {filteredProducts.map((product) => (
                   <ProductCard key={product.id} product={product} />
                 ))}
